a3517f0c5e6cc9ded492108419a33e58ebbf3651
Vitor Silva <vitor.o.silva95@gmail.com>
2022-04-26 21:07:57 -0300

    Refatora componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import InputMovieTitle from './InputMovieTitle';
-import InputMovieSubTitle from './InputMovieSubTitle';
-import InputMovieImage from './InputMovieImage';
-import InputMovieStoryLine from './InputMovieStoryLine';
-import InputMovieRating from './InputMovieRating';
-import InputMovieGenre from './InputMovieGenre';
+import Input from './inputs/Input';
+import InputMovieStoryLine from './inputs/InputMovieStoryLine';
+import Select from './inputs/Select';
 
+const options = ['Ação', 'Comédia', 'Suspense'];
 class AddMovie extends Component {
   constructor() {
     super();
@@ -42,6 +40,86 @@ submitFunction = (state) => {
   });
 }
 
+formsContainerTitleAndSubtitle = (title, subtitle) => (
+  <div className="form-row">
+    <div className="col">
+      <Input
+        label="Titulo"
+        value={ title }
+        className="form-control form-control-sm"
+        name="title"
+        data-testid="title-input"
+        type="text"
+        id="title"
+        onChange={ this.handleState }
+      />
+    </div>
+    <div className="col">
+      <Input
+        label="Subtitulo"
+        className="form-control form-control-sm"
+        name="subtitle"
+        data-testid="subtitle-input"
+        type="text"
+        value={ subtitle }
+        id="subtitle"
+        onChange={ this.handleState }
+      />
+    </div>
+  </div>
+)
+
+formsContainerImageAndGenreAndRating = (imagePath, rating, genre) => (
+  <div className="form-row">
+    <div className="col">
+      <Input
+        label="Imagem"
+        className="form-control form-control-sm"
+        data-testid="image-input"
+        value={ imagePath }
+        name="imagePath"
+        type="text"
+        id="imagePath"
+        onchange={ this.handleState }
+      />
+    </div>
+    <div className="form-group col-md-4">
+      <Input
+        label="Avaliação"
+        className="form-control form-control-sm"
+        name="rating"
+        data-testid="rating-input"
+        type="number"
+        value={ rating }
+        id="rating"
+        onchange={ this.handleState }
+      />
+    </div>
+    <div className="form-group col-md-4">
+      <Input
+        label="Avaliação"
+        className="form-control form-control-sm"
+        name="rating"
+        data-testid="rating-input"
+        type="number"
+        value={ rating }
+        id="rating"
+        onchange={ this.handleState }
+      />
+    </div>
+    <div className="col">
+      <Select
+        label="Gênero"
+        array={ options }
+        className="form-control form-control-sm"
+        name="genre"
+        value={ genre }
+        onChange={ this.handleState }
+      />
+    </div>
+  </div>
+)
+
 render() {
   const { subtitle,
     title,
@@ -52,20 +130,22 @@ render() {
   } = this.state;
 
   return (
-    <form action="" data-testid="add-movie-form">
-      <InputMovieTitle value={ title } funcaoAtualizaPai={ this.handleState } />
-      <InputMovieSubTitle value={ subtitle } funcaoAtualizaPai={ this.handleState } />
-      <InputMovieImage value={ imagePath } funcaoAtualizaPai={ this.handleState } />
-      <InputMovieStoryLine value={ storyline } funcaoAtualizaPai={ this.handleState } />
-      <InputMovieRating value={ rating } funcaoAtualizaPai={ this.handleState } />
-      <InputMovieGenre value={ genre } funcaoAtualizaPai={ this.handleState } />
+    <form action="" data-testid="add-movie-form" div className="forms">
+      {this.formsContainerTitleAndSubtitle(title, subtitle) }
+      {this.formsContainerImageAndGenreAndRating(imagePath, rating, genre)}
+      <InputMovieStoryLine
+        value={ storyline }
+        onChange={ this.handleState }
+      />
       <button
         type="submit"
         data-testid="send-button"
+        className="btn btn-primary"
         onClick={ () => this.submitFunction(this.state) }
       >
         Adicionar filme
       </button>
+
     </form>
   );
 }
