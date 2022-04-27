0f0a4da620a5149e7820f6536b9d107e542df8bb
Vitor Silva <vitor.o.silva95@gmail.com>
2022-04-26 21:06:40 -0300

    Extrai elemento do app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
-import SearchBar from './components/SearchBar';
 import data from './data';
-import MovieList from './components/MovieList';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './pages/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <MovieList movies={ data } />
-      <AddMovie />
+      <MovieLibrary movies={ data } />
     </div>
   );
 }
